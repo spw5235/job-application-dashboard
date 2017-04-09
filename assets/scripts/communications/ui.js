35b1c77e8c7c8b4c319a5c0e2803fcaf9904a0a1
Stephen Wisner <spwisner@gmail.com>
2017-04-09 09:24:21 -0400

    Remove Duplicate Rows Complete

--- assets/scripts/communications/ui.js
+++ assets/scripts/communications/ui.js
@@ -5,11 +5,13 @@ const displayEditCommunication = require('../templates/communication/update-comm
 const displayCommunicationDashboard = require('../templates/communication/get-communications.handlebars');
 const displayCommunicationDetails = require('../templates/communication/show-communication-record.handlebars');
 const displayCommunicationCreateForm = require('../templates/communication/create-communication.handlebars');
+const displaySecondaryTable = require('../templates/communication/secondary-get-communications.handlebars');
 const communicationsApi = require('./api');
 
 const getCommunicationSuccess = (data) => {
   $(".notification-container").children().text("");
   store.communicationDataForEdit = data;
+  console.log(data);
 
   $(".content").children().remove();
 
@@ -18,6 +20,101 @@ const getCommunicationSuccess = (data) => {
   });
 
   $('.content').append(communicationDashboard);
+
+  let communicationSecondaryTable = displaySecondaryTable({
+    communications: data.communications
+  });
+
+  $('.content').append(communicationSecondaryTable);
+
+
+  function removeDuplicateRows($table){
+    function getVisibleRowText($row){
+        return $row.find('td:visible').text().toLowerCase();
+    }
+
+    $table.find('tr').each(function(index, row){
+        let $row = $(row);
+        $row.nextAll('tr').each(function(index, next){
+            let $next = $(next);
+            if(getVisibleRowText($next) == getVisibleRowText($row))
+                $next.remove();
+        })
+    });
+}
+
+removeDuplicateRows($('.secondary-table'));
+
+  // let secondaryData = data.communications;
+  //
+  // let newSecondaryObj = {
+  //   secondary: []
+  // };
+  //
+  // let storeIdArr = [];
+  //
+  // for (let i = 0; i < secondaryData.length; i++ ) {
+  //   storeIdArr.push(secondaryData[i].contact_ref_id);
+  // }
+  //
+  // let uniqueIds = [];
+  //
+  // $.each(storeIdArr, function(i, el){
+  //   if($.inArray(el, uniqueIds) === -1) {
+  //     uniqueIds.push(el);
+  //   }
+  // });
+  //
+  // for (let i = 0; i < secondaryData.length; i++ ) {
+  //   for (let j = 0; j < uniqueIds.length; j++) {
+  //     if (secondaryData[i].contact_ref_id === uniqueIds[j]) {
+  //       newSecondaryObj.secondary.push(secondaryData[i]);
+  //       continue;
+  //     }
+  //     continue;
+  //   }
+  // }
+
+
+  // for (let i = 0; i < uniqueIds.length; i++ ) {
+  //   for (let j = 0; j < secondaryData.length; j++) {
+  //     if (secondaryData[j].contact_ref_id === uniqueIds[i]) {
+  //       newSecondaryObj.secondary.push(secondaryData[j]);
+  //     } else {
+  //       continue;
+  //     }
+  //   }
+  // }
+
+  // for (let j = 0; j < uniqueIds.length; j++) {
+  //
+  //   let currentUniqueId = uniqueIds[j];
+  //   console.log(currentUniqueId);
+  //   for (let i = 0; i < secondaryData.length; i++ ) {
+  //     let currRefIdValue = secondaryData[i].contact_ref_id;
+  //     if (currentUniqueId === currRefIdValue) {
+  //       newSecondaryObj.secondary.push(secondaryData[i]);
+  //       continue;
+  //     }
+  //     continue;
+  //   }
+  //   continue;
+  // }
+
+  // for (let i = 0; i < secondaryData.length; i++ ) {
+  //   let currRefIdValue = secondaryData[i].contact_ref_id;
+  //     for (let j = 0; j < uniqueIds.length; j++) {
+  //       let currentUniqueId = uniqueIds[j];
+  //       if (currentUniqueId === currRefIdValue) {
+  //         newSecondaryObj.secondary.push(secondaryData[i]);
+  //         continue;
+  //       }
+  //       continue;
+  //     }
+  //     continue;
+  // }
+
+  // console.log(newSecondaryObj);
 
 };
 
