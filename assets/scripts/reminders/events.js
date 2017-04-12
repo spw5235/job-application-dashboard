b7326dc91e83eee25e832bec4c9f4cc476519e66
Stephen Wisner <spwisner@gmail.com>
2017-04-12 02:37:06 -0400

    Job Ref in Reminders Complete

--- assets/scripts/reminders/events.js
+++ assets/scripts/reminders/events.js
@@ -3,8 +3,7 @@ const remindersApi = require('./api');
 const remindersUi = require('./ui');
 const getFormFields = require('../../../lib/get-form-fields');
 const store = require('../store');
-const dashboardLogic = require('../dashboard/logic');
-
+const linkLogic = require('../dashboard/link-logic');
 // Reminder EVENTS
 
 const onGetReminders = function(event) {
@@ -22,12 +21,16 @@ const onShowReminderRecord = function(event) {
     .fail(remindersUi.showReminderRecordFailure);
 };
 
-const onDeleteReminder = function(event) {
+const onEditReminder = function(event) {
   event.preventDefault();
-  store.currentReminderId= $(this).attr("data-current-reminder-id");
-  remindersApi.deleteReminder(store.currentReminderId)
-    .done(remindersUi.deleteReminderSuccess)
-    .fail(remindersUi.deleteReminderFailure);
+  store.currentReminderId = $(this).attr("data-current-reminder-id");
+  store.currentJobRefId = $(this).attr("data-current-job-ref-id");
+  store.currentJobRefText = $(this).attr("data-current-job-ref-text");
+
+  // Template
+  let formCategory = "reminder";
+  let listCategory = "job";
+  remindersUi.generateUpdateForm(listCategory, formCategory);
 };
 
 const onCreateReminder = function(event) {
@@ -36,82 +39,69 @@ const onCreateReminder = function(event) {
   store.createReminderData = data;
   store.lastShowReminderData = data;
 
-  // Contact Category
-
-  let contactCategory = "contact-category";
-
-  let contactCategoryId = $("#select-option-contact-category").val();
-
-  if ( contactCategoryId === undefined ) {
-    data.reminder.contact_ref_id = 0;
-  } else {
-    data.reminder.contact_ref_id = parseInt(contactCategoryId);
-  }
-
-  data.reminder.contact_name = dashboardLogic.determineTagText(contactCategory, contactCategoryId);
-
-  // Job Category
-  let jobCategory = "job-category";
-
-  let jobCategoryId = $("#select-option-job-category").val();
-
-  if ( jobCategoryId === undefined ) {
-    data.reminder.job_ref_id = 0;
-  } else {
-    data.reminder.job_ref_id = parseInt(jobCategoryId);
-  }
-
-  data.reminder.job_name = dashboardLogic.determineTagText(jobCategory, jobCategoryId);
-
-  // Reminder Type:
-
   data.reminder.reminder_type = $("#reminder-type-select").val();
 
-  // Submit Values
+  let listCategory = "job";
 
+  let submitValue = linkLogic.obtainOptionVal(listCategory);
+  data.reminder.job_ref_id = parseInt(submitValue);
+
+  let submitText = linkLogic.obtainOptionText(listCategory);
+  data.reminder.job_ref_text = submitText;
+
+  if (submitValue === -1) {
+    data.reminder.job_ref_id = 0;
+    data.reminder.job_ref_text = "";
+  }
+
+  delete data["job-category-radio"];
+  console.log(data);
   remindersApi.createReminder(data)
     .done(remindersUi.createReminderSuccess)
     .fail(remindersUi.createReminderFailure);
 };
 
-const onEditReminder = function(event) {
+const onDeleteReminder = function(event) {
   event.preventDefault();
-  store.currentReminderId = $(this).attr("data-current-reminder-id");
-  remindersUi.updateFormGenerator();
-
-  let categoryContact = "contact-category";
-
-  dashboardLogic.tagCheckboxUpdate(categoryContact);
-
-  let defaultVal = $("#reminder-type-select").attr("data-default-value");
-  let divId = "#reminder-type-select";
-
-  dashboardLogic.preselectDefault(divId, defaultVal);
-
-  let jobId = parseInt($("#insert-tag-job-category").attr("data-update-id"));
-  let jobName = $("#insert-tag-job-category").attr("data-update-name");
-  if (jobId > 0) {
-    $("#job-tag-desc").text("The current job associated with the reminder is: ");
-    $("#insert-tag-job-category").text(jobName);
-  }
-
+  store.currentReminderId= $("#reminder-record-delete").attr("data-current-reminder-id");
+  remindersApi.deleteReminder(store.currentReminderId)
+    .done(remindersUi.deleteReminderSuccess)
+    .fail(remindersUi.deleteReminderFailure);
 };
 
 const onUpdateReminder = function(event) {
   event.preventDefault();
   let data = getFormFields(event.target);
-  let category = "contact-category";
-  let categoryId = $(".select-option-value").val();
 
-  if ( categoryId === undefined ) {
-    data.reminder.contact_ref_id = 0;
+  store.createReminderData = data;
+  store.lastShowReminderData = data;
+
+  let listCategory = "job";
+
+  let refUpdatedDiv = "#" + listCategory + "-update-link";
+
+  let isRefBeingUpdated = $(refUpdatedDiv).prop("checked");
+
+  console.log(isRefBeingUpdated);
+
+  if (isRefBeingUpdated) {
+    let submitValue = linkLogic.obtainOptionVal(listCategory);
+
+    data.reminder.job_ref_id = submitValue;
+
+
+    let submitText = linkLogic.obtainOptionText(listCategory);
+    data.reminder.job_ref_text = submitText;
+
+
+    if (submitValue === -1) {
+      data.reminder.job_ref_id = 0;
+      data.reminder.job_ref_text = "";
+    }
   } else {
-    data.reminder.contact_ref_id = categoryId;
+    data.reminder.job_ref_id = parseInt(store.currentJobRefId);
+    data.reminder.job_ref_text = store.currentJobRefText;
   }
-
-  data.reminder.reminder_type = $("#reminder-type-select").val();
-
-  data.reminder.contact_name = dashboardLogic.determineTagText(category, categoryId);
 
   remindersApi.updateReminder(data)
     .done(remindersUi.updateReminderSuccess)
@@ -123,33 +113,38 @@ const onShowReminderCreateForm = function(event) {
   remindersUi.showReminderCreateForm();
 };
 
-const onSelectReminderDropdown = function(event) {
+const onDisplayJobDropdown = function(event) {
   event.preventDefault();
-  let tagCategory = $(this).attr("class");
-  dashboardLogic.determineApiRequest(tagCategory);
+  let formCategory = "reminder";
+  let listCategory = "job";
+
+  let linkContainerSelect = ".display-dropdown-" + listCategory;
+
+  let altFormContainer = ".display-alt-" + listCategory;
+  let selectVal = parseInt($(this).val());
+
+  if (selectVal === 1) {
+    $(altFormContainer).children().remove();
+    linkLogic.showDropOptionsCreatePage(formCategory, listCategory);
+  } else {
+    $(linkContainerSelect).children().remove();
+    linkLogic.altOptionAppend(formCategory, listCategory);
+  }
 };
 
-const onDisplayReminderDropdown = function(event) {
-  event.preventDefault();
-  let thisCheckBoxStatus = $(this).is(':checked');
-
-  if (!thisCheckBoxStatus) {
-    $(this).parent().children(".tag-select-container").remove();
+const onHideShowUpdateOptions = function() {
+  let isUpdateChecked = $(this).prop("checked");
+  let radioButtonContainer = $(this).parent().parent().parent().children(".update-radio-container-btn");
+  console.log(isUpdateChecked);
+  if ( isUpdateChecked ) {
+    $(".job-radio-container input").prop("checked", false);
+    $(radioButtonContainer).show();
   } else {
-
+    $(".job-radio-container input").prop("checked", false);
+    $("#reminder-ref-text-alt-job-container").remove();
+    $(".display-dropdown-job").children().remove();
+    $(radioButtonContainer).hide();
   }
-
-  let isUpdateForm;
-  let checkboxDivId = $(this).attr("id");
-  let tagCategory = $(this).attr("class");
-  let updateFormStatus = $(".general-form-container").attr("data-update-form");
-  updateFormStatus = parseInt(updateFormStatus);
-  if (updateFormStatus === 1) {
-    isUpdateForm = true;
-  } else {
-    isUpdateForm = false;
-  }
-  dashboardLogic.tagCheckboxClicked(tagCategory, checkboxDivId);
 };
 
 const addHandlers = () => {
@@ -157,16 +152,209 @@ const addHandlers = () => {
   $('.content').on('submit', '#update-reminder-form', onUpdateReminder);
   $('.content').on('click', '#reminder-record-btn-edit', onEditReminder);
   $('.content').on('click', '#generate-create-reminder-btn', onShowReminderCreateForm);
-  $('.content').on('click', '.view-reminder-record-btn', onShowReminderRecord);
-  // $('.content').on('click', '#get-reminders-btn', onGetReminders);
+  $('.content').on('click', '.dashboard-reminder-record-btn', onShowReminderRecord);
   $('#get-reminders-btn').on('click', onGetReminders);
   $('.content').on('click', '#reminder-record-delete', onDeleteReminder);
-  $('.content').on('change', '#tag-contact-to-reminder', onDisplayReminderDropdown);
-  $('.content').on('change', '#tag-job-to-reminder', onDisplayReminderDropdown);
-  $('.content').on('change', '#select-option-contact-category', onSelectReminderDropdown);
-  $('.content').on('change', '#select-option-job-category', onSelectReminderDropdown);
+  $('.content').on('change', '.job-category', onDisplayJobDropdown);
+  $('.content').on('change', "#job-update-link", onHideShowUpdateOptions);
+  $('.content').on('change', '.update-job', onDisplayJobDropdown);
 };
 
 module.exports = {
   addHandlers,
 };
+
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 'use strict';
+// const remindersApi = require('./api');
+// const remindersUi = require('./ui');
+// const getFormFields = require('../../../lib/get-form-fields');
+// const store = require('../store');
+// const dashboardLogic = require('../dashboard/logic');
+//
+// // Reminder EVENTS
+//
+// const onGetReminders = function(event) {
+//   event.preventDefault();
+//   remindersApi.getReminders()
+//     .done(remindersUi.getReminderSuccess)
+//     .fail(remindersUi.getReminderFailure);
+// };
+//
+// const onShowReminderRecord = function(event) {
+//   event.preventDefault();
+//   store.currentReminderId = $(this).attr("data-current-reminder-id");
+//   remindersApi.showReminder()
+//     .done(remindersUi.showReminderRecordSuccess)
+//     .fail(remindersUi.showReminderRecordFailure);
+// };
+//
+// const onDeleteReminder = function(event) {
+//   event.preventDefault();
+//   store.currentReminderId= $(this).attr("data-current-reminder-id");
+//   remindersApi.deleteReminder(store.currentReminderId)
+//     .done(remindersUi.deleteReminderSuccess)
+//     .fail(remindersUi.deleteReminderFailure);
+// };
+//
+// const onCreateReminder = function(event) {
+//   event.preventDefault();
+//   let data = getFormFields(event.target);
+//   store.createReminderData = data;
+//   store.lastShowReminderData = data;
+//
+//   // Contact Category
+//
+//   let contactCategory = "contact-category";
+//
+//   let contactCategoryId = $("#select-option-contact-category").val();
+//
+//   if ( contactCategoryId === undefined ) {
+//     data.reminder.contact_ref_id = 0;
+//   } else {
+//     data.reminder.contact_ref_id = parseInt(contactCategoryId);
+//   }
+//
+//   data.reminder.contact_name = dashboardLogic.determineTagText(contactCategory, contactCategoryId);
+//
+//   // Job Category
+//   let jobCategory = "job-category";
+//
+//   let jobCategoryId = $("#select-option-job-category").val();
+//
+//   if ( jobCategoryId === undefined ) {
+//     data.reminder.job_ref_id = 0;
+//   } else {
+//     data.reminder.job_ref_id = parseInt(jobCategoryId);
+//   }
+//
+//   data.reminder.job_name = dashboardLogic.determineTagText(jobCategory, jobCategoryId);
+//
+//   // Reminder Type:
+//
+//   data.reminder.reminder_type = $("#reminder-type-select").val();
+//
+//   // Submit Values
+//
+//   remindersApi.createReminder(data)
+//     .done(remindersUi.createReminderSuccess)
+//     .fail(remindersUi.createReminderFailure);
+// };
+//
+// const onEditReminder = function(event) {
+//   event.preventDefault();
+//   store.currentReminderId = $(this).attr("data-current-reminder-id");
+//   remindersUi.updateFormGenerator();
+//
+//   let categoryContact = "contact-category";
+//
+//   dashboardLogic.tagCheckboxUpdate(categoryContact);
+//
+//   let defaultVal = $("#reminder-type-select").attr("data-default-value");
+//   let divId = "#reminder-type-select";
+//
+//   dashboardLogic.preselectDefault(divId, defaultVal);
+//
+//   let jobId = parseInt($("#insert-tag-job-category").attr("data-update-id"));
+//   let jobName = $("#insert-tag-job-category").attr("data-update-name");
+//   if (jobId > 0) {
+//     $("#job-tag-desc").text("The current job associated with the reminder is: ");
+//     $("#insert-tag-job-category").text(jobName);
+//   }
+//
+// };
+//
+// const onUpdateReminder = function(event) {
+//   event.preventDefault();
+//   let data = getFormFields(event.target);
+//   let category = "contact-category";
+//   let categoryId = $(".select-option-value").val();
+//
+//   if ( categoryId === undefined ) {
+//     data.reminder.contact_ref_id = 0;
+//   } else {
+//     data.reminder.contact_ref_id = categoryId;
+//   }
+//
+//   data.reminder.reminder_type = $("#reminder-type-select").val();
+//
+//   data.reminder.contact_name = dashboardLogic.determineTagText(category, categoryId);
+//
+//   remindersApi.updateReminder(data)
+//     .done(remindersUi.updateReminderSuccess)
+//     .fail(remindersUi.updateReminderFailure);
+// };
+//
+// const onShowReminderCreateForm = function(event) {
+//   event.preventDefault();
+//   remindersUi.showReminderCreateForm();
+// };
+//
+// const onSelectReminderDropdown = function(event) {
+//   event.preventDefault();
+//   let tagCategory = $(this).attr("class");
+//   dashboardLogic.determineApiRequest(tagCategory);
+// };
+//
+// const onDisplayReminderDropdown = function(event) {
+//   event.preventDefault();
+//   let thisCheckBoxStatus = $(this).is(':checked');
+//
+//   if (!thisCheckBoxStatus) {
+//     $(this).parent().children(".tag-select-container").remove();
+//   } else {
+//
+//   }
+//
+//   let isUpdateForm;
+//   let checkboxDivId = $(this).attr("id");
+//   let tagCategory = $(this).attr("class");
+//   let updateFormStatus = $(".general-form-container").attr("data-update-form");
+//   updateFormStatus = parseInt(updateFormStatus);
+//   if (updateFormStatus === 1) {
+//     isUpdateForm = true;
+//   } else {
+//     isUpdateForm = false;
+//   }
+//   dashboardLogic.tagCheckboxClicked(tagCategory, checkboxDivId);
+// };
+//
+// const addHandlers = () => {
+//   $('.content').on('submit', '#new-reminder-form', onCreateReminder);
+//   $('.content').on('submit', '#update-reminder-form', onUpdateReminder);
+//   $('.content').on('click', '#reminder-record-btn-edit', onEditReminder);
+//   $('.content').on('click', '#generate-create-reminder-btn', onShowReminderCreateForm);
+//   $('.content').on('click', '.view-reminder-record-btn', onShowReminderRecord);
+//   // $('.content').on('click', '#get-reminders-btn', onGetReminders);
+//   $('#get-reminders-btn').on('click', onGetReminders);
+//   $('.content').on('click', '#reminder-record-delete', onDeleteReminder);
+//   $('.content').on('change', '#tag-contact-to-reminder', onDisplayReminderDropdown);
+//   $('.content').on('change', '#tag-job-to-reminder', onDisplayReminderDropdown);
+//   $('.content').on('change', '#select-option-contact-category', onSelectReminderDropdown);
+//   $('.content').on('change', '#select-option-job-category', onSelectReminderDropdown);
+// };
+//
+// module.exports = {
+//   addHandlers,
+// };
--- assets/scripts/reminders/ui.js
+++ assets/scripts/reminders/ui.js
@@ -5,16 +5,14 @@ const displayEditReminder = require('../templates/reminder/update-reminder-form.
 const displayReminderDashboard = require('../templates/reminder/get-reminders.handlebars');
 const displayReminderDetails = require('../templates/reminder/show-reminder-record.handlebars');
 const displayReminderCreateForm = require('../templates/reminder/create-reminder.handlebars');
-const displaySecondaryReminderContact = require('../templates/reminder/secondary-get-reminders-contacts.handlebars');
-
-
 const remindersApi = require('./api');
-const dashboardLogic = require('../dashboard/logic');
+const displayRadioButtonsTemplate = require('../templates/form-template/radio-btn-template.handlebars');
+const displayReminderOptions = require('../templates/reminder/option-dropdown-reminders.handlebars');
+const linkLogic = require('../dashboard/link-logic');
 
 const getReminderSuccess = (data) => {
   $(".notification-container").children().text("");
   store.reminderDataForEdit = data;
-  console.log(data);
 
   $(".content").children().remove();
 
@@ -23,14 +21,6 @@ const getReminderSuccess = (data) => {
   });
 
   $('.content').append(reminderDashboard);
-
-  let secondaryReminderContact = displaySecondaryReminderContact({
-    reminders: data.reminders
-  });
-
-  $('.content').append(secondaryReminderContact);
-
-  dashboardLogic.removeDuplicateRows($('.secondary-table'));
 
 };
 
@@ -51,13 +41,23 @@ const showReminderRecordFailure = () => {
 };
 
 const showReminderCreateForm = () => {
+  let listCategory = "job";
+  let formCategory = "reminder";
+
+
   $(".notification-container").children().text("");
   $(".content").children().remove();
   let showCreateReminderForm = displayReminderCreateForm();
   $('.content').append(showCreateReminderForm);
+
+  let radioTemplate = displayRadioButtonsTemplate();
+  $("#" + listCategory + "-category-radio-container").append(radioTemplate);
+
+  linkLogic.radioClassIdNameGen(formCategory, listCategory);
 };
 
-const updateFormGenerator = function() {
+
+const generateUpdateForm = function(listCategory, formCategory) {
   $(".notification-container").children().text("");
   $(".content").children().remove();
 
@@ -68,6 +68,28 @@ const updateFormGenerator = function() {
   });
   $('.content').append(editReminder);
 
+  let listLinkStatusSelector = "." + listCategory + "-tag-status";
+  let listRefId = parseInt(store.currentJobRefId);
+
+  if (listRefId > 0) {
+    $(listLinkStatusSelector).text("Linked");
+  }
+
+  let updateFormId = "#update-" + formCategory + "-form";
+  let updateFormStatus = parseInt($(updateFormId).attr("data-update-form"));
+
+  if ( updateFormStatus === 1) {
+    let categoryText = "." + listCategory + "-radio-container ";
+    $(categoryText).show();
+    $(".update-radio-container-btn").hide();
+  }
+
+  let currentRefTextValTxt = "." + listCategory + "-update-radio-text";
+
+  if (store.currentJobRefText === "") {
+    $(currentRefTextValTxt).text("N/A");
+  }
+
 };
 
 const getReminderFailure = () => {
@@ -76,16 +98,16 @@ const getReminderFailure = () => {
 };
 
 const createReminderSuccess = (data) => {
+  console.log("createsucces");
   console.log(data);
+  store.currentReminderId = data.reminder.id;
   $(".form-error").text("");
   $(".notification-container").children().text("");
   $(".content").children().remove();
   $(".success-alert").text("Reminder Has Been Successfully Created");
 
-  store.currentReminderId = data.reminder.id;
-
   let showReminderDetails = displayReminderDetails({
-    reminder: data.reminder
+    reminder: store.createReminderData.reminder
   });
   $(".content").append(showReminderDetails);
   $(".current").attr("data-current-reminder-id", store.currentReminderId);
@@ -105,14 +127,40 @@ const deleteReminderFailure = () => {
 };
 
 const updateReminderSuccess = (data) => {
-  $(".notification-container").children().text("");
-  $(".success-alert").text("Reminder Has Been Successfully Updated");
+
   store.currentReminderId = data.reminder.id;
+  $(".form-error").text("");
+  $(".notification-container").children().text("");
   $(".content").children().remove();
+  $(".success-alert").text("Reminder Has Been Successfully Updated");
+
+  let showReminderDetails = displayReminderDetails({
+    reminder: data.reminder
+  });
+  $(".content").append(showReminderDetails);
+  $(".current").attr("data-current-reminder-id", store.currentReminderId);
+};
+
+const displayReminderDropdownSuccess = function(data) {
+  $(".notification-container").children().text("");
+
+  let companyOptionDisplay = displayReminderOptions({
+    reminders: data.reminders
+  });
+
+  let dataUpdateFormVal = parseInt($("#update-reminder-form").attr("data-update-form"));
+
+  $('.associate-reminder-with-reminder-container').append(companyOptionDisplay);
+
+  if (dataUpdateFormVal === 1) {
+    let currentReminderId = store.currentReminderId;
+    let valueString = '#select-option-reminder-name option[value=' + currentReminderId + ']';
+    $(valueString).prop('selected', true);
+  }
+};
+
+const dropDownData = function(data) {
   console.log(data);
-  remindersApi.showReminder()
-    .done(showReminderRecordSuccess)
-    .fail(showReminderRecordFailure);
 };
 
 module.exports = {
@@ -120,10 +168,154 @@ module.exports = {
   showReminderRecordSuccess,
   deleteReminderSuccess,
   deleteReminderFailure,
-  updateFormGenerator,
   showReminderCreateForm,
   getReminderFailure,
   updateReminderSuccess,
   showReminderRecordFailure,
   createReminderSuccess,
+  displayReminderDropdownSuccess,
+  displayReminderOptions,
+  dropDownData,
+  generateUpdateForm,
 };
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 'use strict';
+//
+// const store = require('../store');
+// const displayEditReminder = require('../templates/reminder/update-reminder-form.handlebars');
+// const displayReminderDashboard = require('../templates/reminder/get-reminders.handlebars');
+// const displayReminderDetails = require('../templates/reminder/show-reminder-record.handlebars');
+// const displayReminderCreateForm = require('../templates/reminder/create-reminder.handlebars');
+// const displaySecondaryReminderContact = require('../templates/reminder/secondary-get-reminders-contacts.handlebars');
+//
+//
+// const remindersApi = require('./api');
+// const dashboardLogic = require('../dashboard/logic');
+//
+// const getReminderSuccess = (data) => {
+//   $(".notification-container").children().text("");
+//   store.reminderDataForEdit = data;
+//   console.log(data);
+//
+//   $(".content").children().remove();
+//
+//   let reminderDashboard = displayReminderDashboard({
+//     reminders: data.reminders
+//   });
+//
+//   $('.content').append(reminderDashboard);
+//
+//   let secondaryReminderContact = displaySecondaryReminderContact({
+//     reminders: data.reminders
+//   });
+//
+//   $('.content').append(secondaryReminderContact);
+//
+//   dashboardLogic.removeDuplicateRows($('.secondary-table'));
+//
+// };
+//
+// const showReminderRecordSuccess = (data) => {
+//   $(".notification-container").children().text("");
+//   $(".content").children().remove();
+//   store.lastShowReminderData = data;
+//
+//   let reminderDetails = displayReminderDetails({
+//     reminder: data.reminder
+//   });
+//   $('.content').append(reminderDetails);
+// };
+//
+// const showReminderRecordFailure = () => {
+//   $(".notification-container").children().text("");
+//   console.log('failure');
+// };
+//
+// const showReminderCreateForm = () => {
+//   $(".notification-container").children().text("");
+//   $(".content").children().remove();
+//   let showCreateReminderForm = displayReminderCreateForm();
+//   $('.content').append(showCreateReminderForm);
+// };
+//
+// const updateFormGenerator = function() {
+//   $(".notification-container").children().text("");
+//   $(".content").children().remove();
+//
+//   let data = store.lastShowReminderData;
+//
+//   let editReminder = displayEditReminder({
+//     reminder: data.reminder
+//   });
+//   $('.content').append(editReminder);
+//
+// };
+//
+// const getReminderFailure = () => {
+//   $(".notification-container").children().text("");
+//   console.log('get reminder failure');
+// };
+//
+// const createReminderSuccess = (data) => {
+//   console.log(data);
+//   $(".form-error").text("");
+//   $(".notification-container").children().text("");
+//   $(".content").children().remove();
+//   $(".success-alert").text("Reminder Has Been Successfully Created");
+//
+//   store.currentReminderId = data.reminder.id;
+//
+//   let showReminderDetails = displayReminderDetails({
+//     reminder: data.reminder
+//   });
+//   $(".content").append(showReminderDetails);
+//   $(".current").attr("data-current-reminder-id", store.currentReminderId);
+// };
+//
+// const deleteReminderSuccess = () => {
+//   $(".notification-container").children().text("");
+//   console.log('delete success');
+//   remindersApi.getReminders()
+//     .done(getReminderSuccess)
+//     .fail(getReminderFailure);
+// };
+//
+// const deleteReminderFailure = () => {
+//   $(".notification-container").children().text("");
+//   console.log('delete fail');
+// };
+//
+// const updateReminderSuccess = (data) => {
+//   $(".notification-container").children().text("");
+//   $(".success-alert").text("Reminder Has Been Successfully Updated");
+//   store.currentReminderId = data.reminder.id;
+//   $(".content").children().remove();
+//   console.log(data);
+//   remindersApi.showReminder()
+//     .done(showReminderRecordSuccess)
+//     .fail(showReminderRecordFailure);
+// };
+//
+// module.exports = {
+//   getReminderSuccess,
+//   showReminderRecordSuccess,
+//   deleteReminderSuccess,
+//   deleteReminderFailure,
+//   updateFormGenerator,
+//   showReminderCreateForm,
+//   getReminderFailure,
+//   updateReminderSuccess,
+//   showReminderRecordFailure,
+//   createReminderSuccess,
+// };
